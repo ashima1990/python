3f072b6ac386471eb9eb5ca25ae5b13c5431ba20
ashima1990 <ashima15jain@gmail.com>
2026-08-27 22:23:11 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,26 @@
+print(10)
+n=5
+#print(n)
+print("The value of n is:", n)
+print()
+print("")
+print("WELCOME")
+c = "CODINGAL"
+print(c)
+
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Alex.")
+
+# Printing a fun fact about myself
+# print("I love playing soccer.")
+print("I love playing soccer.")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
+
+a=1
+b=3
+print("The sum of a and b is:", a + b)
